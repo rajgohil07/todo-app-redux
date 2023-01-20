e2b33c1dd010c7d795b60ace5935f23527d00a8d
Raj gohil <raj.famous007@gmail.com>
2023-01-20 16:36:58 +0530

    Log the URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Provider } from "react-redux";
 import { ParentComponent } from "./Components/ParentComponent/ParentComponent";
 import { store } from "./ReduxStore/ReduxStore";
+import { baseURL } from "./Config/BaseURLConfig";
 
 export const App = () => {
+  console.log("baseURL :>> ", baseURL);
   return (
     <Provider store={store}>
       <ParentComponent />
