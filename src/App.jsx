1f77f04c2fb0089a10f25e4a601608dda6d0b4ad
Raj gohil <raj.famous007@gmail.com>
2023-01-20 17:53:10 +0530

    Removed the log of baseURL

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Provider } from "react-redux";
 import { ParentComponent } from "./Components/ParentComponent/ParentComponent";
 import { store } from "./ReduxStore/ReduxStore";
-import { baseURL } from "./Config/BaseURLConfig";
 
 export const App = () => {
-  console.log("baseURL :>> ", baseURL);
   return (
     <Provider store={store}>
       <ParentComponent />
